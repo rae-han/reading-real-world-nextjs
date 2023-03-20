411228204e2b7c453478c8e6e4efa0f3727f144b
raehan <raehan900809@gmail.com>
2023-03-20 19:37:02 +0900

    add: tailwind

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ git push -u origin main
 git remote add origin https://github.com/rae-han/reading-real-world-nextjs.git
 git branch -M main
 git push -u origin main
+
+# for TailWind
+npm i -D autoprefixer postcss tailwindcss
+
+npm i next-themes
+- TailwindCSS는 자바스크립트를 제공하지 않기 때문에 테마 변경에 관련된 로직을 직접 만들어야한다. 이때 next-themes 라이브러리를 사용하면 좀 더 쉽게 구현 가능하다.
+ 
+npx tailwindcss init -p
+- tailwind.config.js: 사용하지 않는 CSS를 제거하거나 TailwindCSS 테마, 어두운 테마 플러그인 등을 설정할 때 사용
+- postcss.config.js: TailwindCSS는 PostCSS를 사용하며 이때 postcss.config.js 파일의 설정을 사용한다. 필요한 경우 이 파일을 수정하여 설정을 변경할 수 있다.
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "next-themes";
+import 'tailwindcss/tailwind.css'
+import TopBar from "@/components/TopBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-gray-900 bg-gray-50 w-full min-h-screen">
+        {/*
+          dark:bg-gray-900 - 다크 테마를 사용할때의 배경 색
+          bg-gray-50  - 기본 배경 색
+          w-full - full-width, 가로 폭을 100%로 한다.
+          min-h-screen 최소 높이 속성값을 전체 화면의 높이 값으로 지정, min-height: 100vh와 같다.
+        */}
+        <TopBar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  )
+
 }
